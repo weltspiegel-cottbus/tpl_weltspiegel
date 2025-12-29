efe1c0d2b51d7062a29feaf08f0ba788ca3c3cf6
Micha Buchholz <mail@lean-stack.rocks>
2025-12-29 22:06:34 +0100

    feature: Style the featured content.

---
+++ html/com_content/featured/default.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_content
+ *
+ * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/** @var \Joomla\Component\Content\Site\View\Featured\HtmlView $this */
+
+?>
+<div class="home-featured">
+    <?php foreach ($this->lead_items as &$item): ?>
+        <?php
+        $this->item = &$item;
+        echo $this->loadTemplate('item');
+        ?>
+    <?php endforeach; ?>
+</div>
