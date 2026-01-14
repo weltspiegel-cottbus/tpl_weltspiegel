6d8507e627721df502eb529f4394b420f400830d
Micha Buchholz <mail@lean-stack.rocks>
2026-01-14 20:59:48 +0100

    refactor: Rename event overrides to cinetix due to upcoming changes.

--- html/com_weltspiegel/cinetixx/default.php
+++ html/com_weltspiegel/cinetixx/default.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Events List View
+ * Cinetixx List View
  * Template override for Weltspiegel template
  *
  * @package     Weltspiegel.Template
@@ -19,10 +19,10 @@ $futureHeadingShown = false;
 
 ?>
 
-<div class="events u-flipped-title-container">
-    <h1 class="events__title u-flipped-title"><?= $this->escape($this->title) ?></h1>
+<div class="cinetixx u-flipped-title-container">
+    <h1 class="cinetixx__title u-flipped-title"><?= $this->escape($this->title) ?></h1>
 
-    <div class="events__list">
+    <div class="cinetixx__list">
         <?php foreach ($this->items as $id => $event): ?>
 
             <?php
@@ -32,7 +32,7 @@ $futureHeadingShown = false;
                     $firstShowDate = new DateTime($event->shows[0]->showStart);
                     $daysUntilFirstShow = $now->diff($firstShowDate)->days;
                     if ($daysUntilFirstShow >= 7) {
-                        echo '<h2 class="events__section-title">Demnächst</h2>';
+                        echo '<h2 class="cinetixx__section-title">Demnächst</h2>';
                         $futureHeadingShown = true;
                     }
                 } catch (Exception $e) {
@@ -40,35 +40,35 @@ $futureHeadingShown = false;
                 }
             }
 
-            $detailRoute = Route::_('index.php?view=event&event_id=' . $id);
+            $detailRoute = Route::_('index.php?view=cinetixxitem&event_id=' . $id);
             ?>
 
-            <article class="event-card">
-                <div class="event-card__poster">
-                    <a href="<?= $detailRoute ?>" class="event-card__poster-link">
+            <article class="cinetixx-card">
+                <div class="cinetixx-card__poster">
+                    <a href="<?= $detailRoute ?>" class="cinetixx-card__poster-link">
                         <img src="<?= htmlspecialchars($event->poster) ?>"
                              alt="Filmplakat <?= $this->escape($event->title) ?>"
-                             class="event-card__poster-img">
+                             class="cinetixx-card__poster-img">
                     </a>
                 </div>
 
-                <h2 class="event-card__title">
-                    <a href="<?= $detailRoute ?>" class="event-card__title-link">
+                <h2 class="cinetixx-card__title">
+                    <a href="<?= $detailRoute ?>" class="cinetixx-card__title-link">
                         <?= $this->escape($event->title) ?>
                     </a>
                 </h2>
 
-                <div class="event-card__description">
+                <div class="cinetixx-card__description">
                     <?= $event->text ?>
                 </div>
 
-                <div class="event-card__details">
+                <div class="cinetixx-card__details">
                     Dauer: <?= htmlspecialchars($event->duration) ?>,
                     Sprache: <?= htmlspecialchars($event->languageShort) ?>,
                     FSK: <?= htmlspecialchars($event->fsk) ?>
                 </div>
 
-                <div class="event-card__showbox">
+                <div class="cinetixx-card__showbox">
                     <?= LayoutHelper::render('booking.showbox', $event) ?>
                 </div>
             </article>
--- html/com_weltspiegel/cinetixxitem/default.php
+++ html/com_weltspiegel/cinetixxitem/default.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Single Event View
+ * Single Cinetixx Item View
  * Template override for Weltspiegel template
  *
  * @package     Weltspiegel.Template
@@ -16,43 +16,43 @@ use Joomla\CMS\Layout\LayoutHelper;
 $event = $this->item;
 ?>
 
-<article class="event">
-    <h1 class="event__title"><?= $this->escape($this->title) ?></h1>
+<article class="cinetixxitem">
+    <h1 class="cinetixxitem__title"><?= $this->escape($this->title) ?></h1>
 
-    <div class="event__poster">
+    <div class="cinetixxitem__poster">
         <img src="<?= htmlspecialchars($event->poster) ?>"
              alt="Filmplakat <?= $this->escape($event->title) ?>"
-             class="event__poster-img">
+             class="cinetixxitem__poster-img">
     </div>
 
-    <div class="event__details">
+    <div class="cinetixxitem__details">
         Dauer: <?= htmlspecialchars($event->duration) ?>,
         Sprache: <?= htmlspecialchars($event->languageShort) ?>,
         FSK: <?= htmlspecialchars($event->fsk) ?>
     </div>
 
-    <div class="event__showbox">
+    <div class="cinetixxitem__showbox">
         <?= LayoutHelper::render('booking.showbox', $event) ?>
     </div>
 
-    <div class="event__description">
+    <div class="cinetixxitem__description">
         <?= $event->text ?>
     </div>
 
     <?php if (!empty($event->trailerId)): ?>
-        <div class="event__trailer">
+        <div class="cinetixxitem__trailer">
             <?= LayoutHelper::render('youtube.embed', ['videoId' => $event->trailerId]) ?>
         </div>
     <?php endif; ?>
 
     <?php if (!empty($event->images)): ?>
-        <div class="event__gallery">
-            <ul class="event__gallery-list">
+        <div class="cinetixxitem__gallery">
+            <ul class="cinetixxitem__gallery-list">
                 <?php foreach ($event->images as $index => $image): ?>
-                    <li class="event__gallery-item">
+                    <li class="cinetixxitem__gallery-item">
                         <img src="<?= htmlspecialchars($image) ?>"
                              alt="Filmbild <?= ($index + 1) ?> zu <?= $this->escape($event->title) ?>"
-                             class="event__gallery-img">
+                             class="cinetixxitem__gallery-img">
                     </li>
                 <?php endforeach; ?>
             </ul>
