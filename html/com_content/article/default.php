7528ae98a3b37e89a5927169244e95b2c0f5daa8
Micha Buchholz <mail@lean-stack.rocks>
2026-01-19 22:52:12 +0100

    refactor: Start aliging layout with cinetixx layout. Cleaner seperation of custom articles.

--- html/com_content/article/default.php
+++ html/com_content/article/default.php
@@ -17,75 +17,70 @@ use Joomla\CMS\Layout\LayoutHelper;
 // Check if this is a Vorschau article (category 8)
 $isVorschau = ($this->item->catid == 8);
 
-// If Vorschau, check if it's component-generated (has YouTube URL or images)
-$attribs = json_decode($this->item->attribs, true);
-$hasYouTubeTrailer = !empty($attribs['youtube_url']);
-$images = json_decode($this->item->images, true);
-$hasBannerImage = !empty($images['image_fulltext']) || !empty($images['image_intro']);
+// Parse article attributes
+$attribs = json_decode($this->item->attribs, true) ?: [];
+$images = json_decode($this->item->images, true) ?: [];
 
-$isComponentVorschau = $isVorschau && ($hasYouTubeTrailer || $hasBannerImage);
+// Component-managed Vorschau has source marker
+$isComponentVorschau = $isVorschau && (!empty($attribs['source']) && $attribs['source'] === 'com_weltspiegel');
+
+// Check for optional content elements
+$hasYouTubeTrailer = !empty($attribs['youtube_url']);
+$hasBannerImage = !empty($images['image_fulltext']) || !empty($images['image_intro']);
 ?>
 
 <?php if ($isComponentVorschau): ?>
     <!-- Component-generated Vorschau article -->
-    <article class="vorschau vorschau--full">
+    <article class="vorschau vorschau--full u-flipped-title-container">
         <?php if ($this->params->get('show_title')): ?>
-            <header class="vorschau__header">
-                <h1 class="vorschau__title"><?php echo $this->escape($this->item->title); ?></h1>
-            </header>
+            <h1 class="vorschau__title u-flipped-title"><?= $this->escape($this->item->title) ?></h1>
         <?php endif; ?>
 
         <?php if ($hasBannerImage): ?>
             <div class="vorschau__banner">
                 <?php
                 $bannerImage = !empty($images['image_fulltext']) ? $images['image_fulltext'] : $images['image_intro'];
-                $bannerAlt = !empty($images['image_fulltext_alt']) ? $images['image_fulltext_alt'] : $images['image_intro_alt'];
+                $bannerAlt = !empty($images['image_fulltext_alt']) ? $images['image_fulltext_alt'] : ($images['image_intro_alt'] ?? '');
                 ?>
-                <img src="<?php echo htmlspecialchars($bannerImage); ?>"
-                     alt="<?php echo htmlspecialchars($bannerAlt ?: $this->item->title); ?>"
+                <img src="<?= htmlspecialchars($bannerImage) ?>"
+                     alt="<?= htmlspecialchars($bannerAlt ?: $this->item->title) ?>"
                      class="vorschau__banner-image">
             </div>
         <?php endif; ?>
 
-        <div class="vorschau__content">
-            <?php echo $this->item->event->afterDisplayTitle; ?>
-            <?php echo $this->item->event->beforeDisplayContent; ?>
+        <?= $this->item->event->afterDisplayTitle ?>
+        <?= $this->item->event->beforeDisplayContent ?>
 
-            <div class="vorschau__body">
-                <?php echo $this->item->fulltext ?: $this->item->introtext; ?>
-            </div>
-
-            <?php if ($hasYouTubeTrailer): ?>
-                <div class="vorschau__trailer">
-                    <?php echo LayoutHelper::render('com_weltspiegel.youtube.embed', [
-                        'videoId' => $attribs['youtube_url']
-                    ]); ?>
-                </div>
-            <?php endif; ?>
-
-            <?php echo $this->item->event->afterDisplayContent; ?>
+        <div class="vorschau__body">
+            <?= $this->item->fulltext ?: $this->item->introtext ?>
         </div>
+
+        <?php if ($hasYouTubeTrailer): ?>
+            <div class="vorschau__trailer">
+                <?= LayoutHelper::render('com_weltspiegel.youtube.embed', [
+                    'videoId' => $attribs['youtube_url']
+                ]) ?>
+            </div>
+        <?php endif; ?>
+
+        <?= $this->item->event->afterDisplayContent ?>
     </article>
 
 <?php elseif ($isVorschau): ?>
-    <!-- Freeform Vorschau article (minimal boxed style) -->
-    <article class="vorschau vorschau--simple">
+    <!-- Freeform Vorschau article (minimal style) -->
+    <article class="vorschau vorschau--simple u-flipped-title-container">
         <?php if ($this->params->get('show_title')): ?>
-            <header class="vorschau__header">
-                <h1 class="vorschau__title"><?php echo $this->escape($this->item->title); ?></h1>
-            </header>
+            <h1 class="vorschau__title u-flipped-title"><?= $this->escape($this->item->title) ?></h1>
         <?php endif; ?>
 
-        <div class="vorschau__content">
-            <?php echo $this->item->event->afterDisplayTitle; ?>
-            <?php echo $this->item->event->beforeDisplayContent; ?>
+        <?= $this->item->event->afterDisplayTitle ?>
+        <?= $this->item->event->beforeDisplayContent ?>
 
-            <div class="vorschau__body">
-                <?php echo $this->item->text; ?>
-            </div>
-
-            <?php echo $this->item->event->afterDisplayContent; ?>
+        <div class="vorschau__body">
+            <?= $this->item->text ?>
         </div>
+
+        <?= $this->item->event->afterDisplayContent ?>
     </article>
 
 <?php else: ?>
--- html/com_content/category/blog.php
+++ html/com_content/category/blog.php
@@ -1,6 +1,9 @@
 <?php
 
 /**
+ * Category Blog View
+ * Template override for Weltspiegel template
+ *
  * @package     Weltspiegel.Template
  * @subpackage  com_content
  *
@@ -11,6 +14,8 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Router\Route;
+use Joomla\Component\Content\Site\Helper\RouteHelper;
 
 /** @var Joomla\Component\Content\Site\View\Category\HtmlView $this */
 
@@ -20,101 +25,67 @@ $isVorschauCategory = ($this->category->id == 8);
 
 <?php if ($isVorschauCategory): ?>
     <!-- Vorschau category blog -->
-    <div class="vorschau-list">
-        <?php if ($this->params->get('show_category_title', 1) || $this->params->get('page_subheading')): ?>
-            <header class="vorschau-list__header u-flipped-title-container">
-                <h1 class="vorschau-list__title u-flipped-title">
-                    <?php echo $this->escape($this->params->get('page_subheading') ?: $this->category->title); ?>
-                </h1>
-            </header>
-        <?php endif; ?>
+    <div class="vorschau-list u-flipped-title-container">
+        <h1 class="vorschau-list__title u-flipped-title">
+            <?= $this->escape($this->params->get('page_subheading') ?: $this->category->title) ?>
+        </h1>
 
         <?php if ($this->params->get('show_description', 1) && !empty($this->category->description)): ?>
             <div class="vorschau-list__description">
-                <?php echo $this->category->description; ?>
+                <?= $this->category->description ?>
             </div>
         <?php endif; ?>
 
-        <div class="vorschau-grid">
+        <div class="vorschau-list__items">
             <?php foreach ($this->items as $item): ?>
                 <?php
-                // Check if component-generated
+                // Check if component-generated (has image or YouTube)
                 $attribs = json_decode($item->attribs, true);
                 $hasYouTubeTrailer = !empty($attribs['youtube_url']);
                 $images = json_decode($item->images, true);
-                $hasBannerImage = !empty($images['image_intro']) || !empty($images['image_fulltext']);
-                $isComponentVorschau = $hasYouTubeTrailer || $hasBannerImage;
+                $posterImage = !empty($images['image_intro']) ? $images['image_intro'] : ($images['image_fulltext'] ?? '');
+                $posterAlt = !empty($images['image_intro_alt']) ? $images['image_intro_alt'] : ($images['image_fulltext_alt'] ?? '');
+                $hasPoster = !empty($posterImage);
                 ?>
 
-                <?php if ($isComponentVorschau): ?>
-                    <!-- Component-generated Vorschau card -->
-                    <article class="vorschau-card vorschau-card--component">
-                        <?php if ($hasBannerImage): ?>
-                            <div class="vorschau-card__image">
-                                <?php
-                                $posterImage = !empty($images['image_intro']) ? $images['image_intro'] : $images['image_fulltext'];
-                                $posterAlt = !empty($images['image_intro_alt']) ? $images['image_intro_alt'] : $images['image_fulltext_alt'];
-                                ?>
-                                <a href="<?php echo $item->link; ?>">
-                                    <img src="<?php echo htmlspecialchars($posterImage); ?>"
-                                         alt="<?php echo htmlspecialchars($posterAlt ?: $item->title); ?>"
-                                         class="vorschau-card__poster">
-                                </a>
-                            </div>
-                        <?php endif; ?>
-
-                        <div class="vorschau-card__content">
-                            <h2 class="vorschau-card__title">
-                                <a href="<?php echo $item->link; ?>">
-                                    <?php echo $this->escape($item->title); ?>
-                                </a>
-                            </h2>
-
-                            <?php if (!empty($item->introtext)): ?>
-                                <div class="vorschau-card__intro">
-                                    <?php echo $item->introtext; ?>
-                                </div>
-                            <?php endif; ?>
-
-                            <a href="<?php echo $item->link; ?>" class="vorschau-card__more">
-                                [[ mehr ]]
+                <?php
+                $articleLink = Route::_(RouteHelper::getArticleRoute($item->slug, $item->catid, $item->language));
+                ?>
+                <article class="vorschau-card<?= $hasPoster ? '' : ' vorschau-card--no-poster' ?>">
+                    <?php if ($hasPoster): ?>
+                        <div class="vorschau-card__poster">
+                            <a href="<?= $articleLink ?>" class="vorschau-card__poster-link">
+                                <img src="<?= htmlspecialchars($posterImage) ?>"
+                                     alt="<?= htmlspecialchars($posterAlt ?: $item->title) ?>"
+                                     class="vorschau-card__poster-img">
                             </a>
                         </div>
-                    </article>
+                    <?php endif; ?>
 
-                <?php else: ?>
-                    <!-- Freeform Vorschau card -->
-                    <article class="vorschau-card vorschau-card--simple">
-                        <div class="vorschau-card__content">
-                            <h2 class="vorschau-card__title">
-                                <a href="<?php echo $item->link; ?>">
-                                    <?php echo $this->escape($item->title); ?>
-                                </a>
-                            </h2>
+                    <h2 class="vorschau-card__title">
+                        <a href="<?= $articleLink ?>" class="vorschau-card__title-link">
+                            <?= $this->escape($item->title) ?>
+                        </a>
+                    </h2>
 
-                            <?php if (!empty($item->introtext)): ?>
-                                <div class="vorschau-card__intro">
-                                    <?php echo $item->introtext; ?>
-                                </div>
-                            <?php endif; ?>
-
-                            <a href="<?php echo $item->link; ?>" class="vorschau-card__more">
-                                [[ mehr ]]
-                            </a>
+                    <?php if (!empty($item->introtext)): ?>
+                        <div class="vorschau-card__description">
+                            <?= $item->introtext ?>
                         </div>
-                    </article>
-                <?php endif; ?>
+                    <?php endif; ?>
+                </article>
+
             <?php endforeach; ?>
         </div>
 
         <?php if (($this->params->def('show_pagination', 1) == 1 || ($this->params->get('show_pagination') == 2)) && ($this->pagination->pagesTotal > 1)): ?>
             <div class="vorschau-list__pagination">
-                <?php echo $this->pagination->getPagesLinks(); ?>
+                <?= $this->pagination->getPagesLinks() ?>
             </div>
         <?php endif; ?>
     </div>
 
 <?php else: ?>
     <!-- Standard category blog (not Vorschau) -->
-    <?php echo LayoutHelper::render('joomla.content.category_default', $this); ?>
+    <?= LayoutHelper::render('joomla.content.category_default', $this) ?>
 <?php endif; ?>
