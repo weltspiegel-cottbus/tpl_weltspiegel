788dd3705cd109a4356fadb85ae70ce69d6039a9
Micha Buchholz <mail@lean-stack.rocks>
2026-01-20 22:00:04 +0100

    refactor: Add an empty category view state.

--- html/com_content/category/blog.php
+++ html/com_content/category/blog.php
@@ -21,6 +21,12 @@ use Joomla\Component\Content\Site\Helper\RouteHelper;
 
 // Categories using card layout (Vorschauen: 8, Veranstaltungen: 9)
 $useCardLayout = in_array($this->category->id, [8, 9]);
+
+// Empty state messages per category
+$emptyMessages = [
+    8 => 'Zur Zeit sind keine Vorschauen verfügbar.',
+    9 => 'Zur Zeit sind keine Veranstaltungen geplant.',
+];
 ?>
 
 <?php if ($useCardLayout): ?>
@@ -36,47 +42,53 @@ $useCardLayout = in_array($this->category->id, [8, 9]);
             </div>
         <?php endif; ?>
 
-        <div class="content-list__items">
-            <?php foreach ($this->items as $item): ?>
-                <?php
-                $attribs = json_decode($item->attribs, true);
-                $images = json_decode($item->images, true);
-                $posterImage = !empty($images['image_intro']) ? $images['image_intro'] : ($images['image_fulltext'] ?? '');
-                $posterAlt = !empty($images['image_intro_alt']) ? $images['image_intro_alt'] : ($images['image_fulltext_alt'] ?? '');
-                $hasPoster = !empty($posterImage);
-                $articleLink = Route::_(RouteHelper::getArticleRoute($item->slug, $item->catid, $item->language));
-                ?>
-                <article class="content-card<?= $hasPoster ? '' : ' content-card--no-poster' ?>">
-                    <?php if ($hasPoster): ?>
-                        <div class="content-card__poster">
-                            <a href="<?= $articleLink ?>" class="content-card__poster-link">
-                                <img src="<?= htmlspecialchars($posterImage) ?>"
-                                     alt="<?= htmlspecialchars($posterAlt ?: $item->title) ?>"
-                                     class="content-card__poster-img">
-                            </a>
-                        </div>
-                    <?php endif; ?>
-
-                    <h2 class="content-card__title">
-                        <a href="<?= $articleLink ?>" class="content-card__title-link">
-                            <?= $this->escape($item->title) ?>
-                        </a>
-                    </h2>
-
-                    <?php if (!empty($item->introtext)): ?>
-                        <div class="content-card__description">
-                            <?= $item->introtext ?>
-                        </div>
-                    <?php endif; ?>
-                </article>
-
-            <?php endforeach; ?>
-        </div>
-
-        <?php if (($this->params->def('show_pagination', 1) == 1 || ($this->params->get('show_pagination') == 2)) && ($this->pagination->pagesTotal > 1)): ?>
-            <div class="content-list__pagination">
-                <?= $this->pagination->getPagesLinks() ?>
+        <?php if (empty($this->items)): ?>
+            <div class="content-list__empty">
+                <p><?= $emptyMessages[$this->category->id] ?? 'Zur Zeit sind keine Einträge vorhanden.' ?></p>
             </div>
+        <?php else: ?>
+            <div class="content-list__items">
+                <?php foreach ($this->items as $item): ?>
+                    <?php
+                    $attribs = json_decode($item->attribs, true);
+                    $images = json_decode($item->images, true);
+                    $posterImage = !empty($images['image_intro']) ? $images['image_intro'] : ($images['image_fulltext'] ?? '');
+                    $posterAlt = !empty($images['image_intro_alt']) ? $images['image_intro_alt'] : ($images['image_fulltext_alt'] ?? '');
+                    $hasPoster = !empty($posterImage);
+                    $articleLink = Route::_(RouteHelper::getArticleRoute($item->slug, $item->catid, $item->language));
+                    ?>
+                    <article class="content-card<?= $hasPoster ? '' : ' content-card--no-poster' ?>">
+                        <?php if ($hasPoster): ?>
+                            <div class="content-card__poster">
+                                <a href="<?= $articleLink ?>" class="content-card__poster-link">
+                                    <img src="<?= htmlspecialchars($posterImage) ?>"
+                                         alt="<?= htmlspecialchars($posterAlt ?: $item->title) ?>"
+                                         class="content-card__poster-img">
+                                </a>
+                            </div>
+                        <?php endif; ?>
+
+                        <h2 class="content-card__title">
+                            <a href="<?= $articleLink ?>" class="content-card__title-link">
+                                <?= $this->escape($item->title) ?>
+                            </a>
+                        </h2>
+
+                        <?php if (!empty($item->introtext)): ?>
+                            <div class="content-card__description">
+                                <?= $item->introtext ?>
+                            </div>
+                        <?php endif; ?>
+                    </article>
+
+                <?php endforeach; ?>
+            </div>
+
+            <?php if (($this->params->def('show_pagination', 1) == 1 || ($this->params->get('show_pagination') == 2)) && ($this->pagination->pagesTotal > 1)): ?>
+                <div class="content-list__pagination">
+                    <?= $this->pagination->getPagesLinks() ?>
+                </div>
+            <?php endif; ?>
         <?php endif; ?>
     </div>
 
